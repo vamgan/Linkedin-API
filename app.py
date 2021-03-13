7eeb8292c3b9868f43e0f0e372f6f023f51f4a13
Vamil Gandhi <hello@vamilgandhi.com>
2021-03-13 02:30:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -13,4 +13,6 @@ def jobs():
     elif request.method == 'GET':
         return jsonify(getJob(request.args.get('role')))
 
-app.run(debug=True)
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
