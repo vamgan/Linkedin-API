05cf1bc3a544b41d7e17d92b14aadf43443c05d7
Vamil Gandhi <hello@vamilgandhi.com>
2021-03-15 20:47:49 -0400

    Update app.py

--- app.py
+++ app.py
@@ -15,4 +15,4 @@ def jobs():
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True,port = process.env.PORT)
+    app.run(threaded=True)
